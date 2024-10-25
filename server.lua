ff51c5a981d6ff33410a204ead87d36ba2d7918f
Gabriel Alcântara <gabrielqalcantara@gmail.com>
2024-10-24 23:25:03 -0300

    Creating webserver for take the requests
    idk what is happening here

--- server.lua
+++ server.lua
@@ -1,9 +1,37 @@
-local Pegasus = require 'pegasus'
+local app = require "milua"
+require("read")
+local usr = io.open("active-users.lua","a+")
+io.output(usr)
+app.add_handler(
+  "GET",
+  "/",
+  function()
+    return "{\"date\":\"23\",\"user\":\"fc165869-9125-4674-bb46-ac6d0bd308ec\"}", {
+      ["Content-Type"] = "text/json"
+    }
+  end
 
-local server = Pegasus:new()
+)
 
-server:start(function (req, res)
-  res:addHeaders('p', 'text/html', ' asasdas'):write('hsdasdasdasdasdasdello pegasus world!')
+app.add_handler(
+    "POST",
+    "/jsoncc",
+    function()
+        return io.write("a"), {
+      ["Content-Type"] = "text/plain"
+    }
+    end
+)
 
-  return res:close()
-end)
+
+
+
+
+
+local config = {
+  HOST = "127.0.0.1",
+  PORT = "9213"
+
+}
+
+app.start(config)
